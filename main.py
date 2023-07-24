7515ec96965669c053156cf9f93d792611073d5d
LuznyKamus1 <r3kinek0@gmail.com>
2023-07-24 14:49:11 +0200

    i used black to format the code but almost no changes

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from random import randrange
 
+
 def encrypt(s: str, key: [int]):
     encrypted = []
-    _i=0
+    _i = 0
     for byte in to_bytes(s):
-        encrypted.append(byte*key[_i])
-        _i+=1
+        encrypted.append(byte * key[_i])
+        _i += 1
     return encrypted
 
-def decrypt(encrypted: [int], key: [int]) -> [int] :
+
+def decrypt(encrypted: [int], key: [int]) -> [int]:
     for i in range(0, len(key)):
-        encrypted[i]=int(encrypted[i]/key[i])
+        encrypted[i] = int(encrypted[i] / key[i])
     return encrypted
+
 
 def gen_key(s: str):
     key = []
     num = 0
     for _ in range(0, len(s)):
-        num=randrange(-10, 10)
+        num = randrange(-10, 10)
         while num == 0:
             num = randrange(-10, 10)
         key.append(int(num))
     return key
+
 
 def to_bytes(s: str):
     result = []
@@ -30,26 +34,28 @@ def to_bytes(s: str):
 
     return result
 
+
 def from_bytes(bytes: [int]) -> str:
     result = ""
     for byte in bytes:
-        try: 
+        try:
             result += chr(byte)
         except ValueError:
             result += chr(0)
     return result
 
-s="asa to dobry pies!"
+
+s = "asa to dobry pies!"
 key = gen_key(s)
 
 print("string to encrypt: {}".format(s))
 print("encryption key: {}".format(key))
-encrypted=encrypt(s, key)
+encrypted = encrypt(s, key)
 
 print("encrypted bytes: {}".format(encrypted))
 print("encrypted string: {}".format(from_bytes(encrypted)))
 
-decrypted=decrypt(encrypted, key)
+decrypted = decrypt(encrypted, key)
 print("decrypted bytes: {}".format(decrypted))
 
 print("decrypted string: {}".format(from_bytes(decrypted)))
